216c44b2f140f0ffb6deea3e32a8cc5eb32a58c5
Chufretalas <marcobene.contact@gmail.com>
2023-03-04 21:53:32 -0300

    tested command invoking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api/tauri";
+    let name: string = "";
+    let greetMsg: string = "";
+</script>
+
+<main class="main-class">
+    <input type="text" name="" id="" bind:value={name} />
+    <button
+        on:click={async () => {
+            greetMsg = await invoke("greet", { name });
+        }}>Click here</button
+    >
+    <button
+        on:click={() => {
+            name = "";
+            greetMsg = "";
+        }}>Clear</button
+    >
+    <span>{greetMsg}</span>
+</main>
+
+<style>
+    .main-class {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        row-gap: 1rem;
+        font-size: 2rem;
+    }
+</style>
